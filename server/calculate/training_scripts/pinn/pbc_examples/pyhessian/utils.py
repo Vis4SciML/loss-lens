f1f3c7683eef6ad0cb93953b86c99de7eec0f99c
Tiankai Xie <42656139+tiankaixie@users.noreply.github.com>
2024-01-22 23:02:18 -0700

    calculate2 calculate

--- server/calculate/training_scripts/pinn/pbc_examples/pyhessian/utils.py
+++ server/calculate/training_scripts/pinn/pbc_examples/pyhessian/utils.py
@@ -18,80 +18,95 @@
 # along with PyHessian.  If not, see <http://www.gnu.org/licenses/>.
 #*
 
-import torch
-import math
-from torch.autograd import Variable
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
 
 
-def group_product(xs, ys):
+def getData(name='cifar10', train_bs=128, test_bs=1000):
     """
-    the inner product of two lists of variables xs,ys
-    :param xs:
-    :param ys:
-    :return:
+    Get the dataloader
     """
-    return sum([torch.sum(x * y) for (x, y) in zip(xs, ys)])
+    if name == 'cifar10':
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+
+        trainset = datasets.CIFAR10(root='../data',
+                                    train=True,
+                                    download=True,
+                                    transform=transform_train)
+        train_loader = torch.utils.data.DataLoader(trainset,
+                                                   batch_size=train_bs,
+                                                   shuffle=True)
+
+        testset = datasets.CIFAR10(root='../data',
+                                   train=False,
+                                   download=False,
+                                   transform=transform_test)
+        test_loader = torch.utils.data.DataLoader(testset,
+                                                  batch_size=test_bs,
+                                                  shuffle=False)
+    if name == 'cifar10_without_dataaugmentation':
+        transform_train = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+
+        trainset = datasets.CIFAR10(root='../data',
+                                    train=True,
+                                    download=True,
+                                    transform=transform_train)
+        train_loader = torch.utils.data.DataLoader(trainset,
+                                                   batch_size=train_bs,
+                                                   shuffle=True)
+
+        testset = datasets.CIFAR10(root='../data',
+                                   train=False,
+                                   download=False,
+                                   transform=transform_test)
+        test_loader = torch.utils.data.DataLoader(testset,
+                                                  batch_size=test_bs,
+                                                  shuffle=False)
+
+    return train_loader, test_loader
 
 
-def group_add(params, update, alpha=1):
+def test(model, test_loader, cuda=True):
     """
-    params = params + update*alpha
-    :param params: list of variable
-    :param update: list of data
-    :return:
+    Get the test performance
     """
-    for i, p in enumerate(params):
-        params[i].data.add_(update[i] * alpha)
-    return params
-
-
-def normalization(v):
-    """
-    normalization of a list of vectors
-    return: normalized vectors v
-    """
-    s = group_product(v, v)
-    s = s**0.5
-    s = s.cpu().item()
-    v = [vi / (s + 1e-6) for vi in v]
-    return v
-
-
-def get_params_grad(model):
-    """
-    get model parameters and corresponding gradients
-    """
-    params = []
-    grads = []
-    for param in model.parameters():
-        if not param.requires_grad:
-            continue
-        params.append(param)
-        grads.append(0. if param.grad is None else param.grad + 0.)
-    return params, grads
-
-
-def hessian_vector_product(gradsH, params, v):
-    """
-    compute the hessian vector product of Hv, where
-    gradsH is the gradient at the current point,
-    params is the corresponding variables,
-    v is the vector.
-    """
-    hv = torch.autograd.grad(gradsH,
-                             params,
-                             grad_outputs=v,
-                             only_inputs=True,
-                             retain_graph=True)
-    return hv
-
-
-def orthnormal(w, v_list):
-    """
-    make vector w orthogonal to each vector in v_list.
-    afterwards, normalize the output w
-    """
-    for v in v_list:
-        w = group_add(w, v, alpha=-group_product(w, v))
-    return normalization(w)
+    model.eval()
+    correct = 0
+    total_num = 0
+    for data, target in test_loader:
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+        output = model(data)
+        pred = output.data.max(
+            1, keepdim=True)[1]  # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum().item()
+        total_num += len(data)
+    print('testing_correct: ', correct / total_num, '\n')
+    return correct / total_num
